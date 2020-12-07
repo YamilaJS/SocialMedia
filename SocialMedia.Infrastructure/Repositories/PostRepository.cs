2c7eb5ef591e65cc6199da89c402b16d8909ca4c
Leonardo Larrosa <llarrosa@siembro.com>
2020-12-07 00:54:30 -0300

    Added PostController and PostRepository

--- SocialMedia.Infrastructure/Repositories/PostRepository.cs
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -1,7 +1,21 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
 namespace SocialMedia.Infrastructure.Repositories
 {
     public class PostRepository
     {
-        
+        public IEnumerable<Post> GetPosts()
+        {
+            var posts = Enumerable.Range(1,10).Select(x => new Post
+            {
+                PostId = x,
+                UserId = x * 2,
+                Date = DateTime.Now,
+                Description = $"Description {x}",
+                Image = $"https://misapis.com/{x}"
+            });
+        }
     }
 }
